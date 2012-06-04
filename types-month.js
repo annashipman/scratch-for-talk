174700b0af14503e77e2144bc79c56b7aa39b01d
Hibri Marzook <hibri@hibri.net>
2012-06-04 15:05:58 +0100

    add hover in and out animation

--- types-month.js
+++ types-month.js
@@ -15,8 +15,35 @@ function renderTypesByMonth(paper){
 
 	var data = [numberOfFeatures, numberOfBugs, numberOfMaintenanceTasks, numberOfInfrastructureTasks];
 	pie = paper.piechart(xPos, yPos, radius, data , {colors: colorPalette, label: data});
+	pie.hover(hoverIn, hoverOut);
 	paper.text(xPos, yPos +radius + 10, moment.monthsShort[0] + " " + year  )
 }
+
+function hoverIn(){
+	//stop scale animation
+	this.sector.stop();
+	//create a popup and add to the flag object so we can clear it later
+	this.flag = paper.popup(this.mx, this.my, this.value.value)
+				.attr({
+						fill: "0-#c9de96-#8ab66b:44-#398235",
+						stroke: "#000"});
+	this.sector.scale(1.5, 1.5, this.cx, this.cy);
+	
+    if (this.label) {
+        this.label[0].stop();
+        this.label[0].attr({ r: 7.5 });
+        this.label[1].attr({ "font-weight": 800 });
+    }
+ }
+
+function hoverOut(){
+	this.flag.animate({opacity: 0}, 300, function () {this.remove();});
+	this.sector.animate({ transform: 's1 1 ' + this.cx + ' ' + this.cy }, 500, "bounce");
+	if (this.label) {
+    	this.label[0].animate({ r: 5 }, 500, "bounce");
+        this.label[1].attr({ "font-weight": 400 });
+    }
+ }
 
 function countCards(year, month, cardType){
 	//count the number of entries, that were created on Option
