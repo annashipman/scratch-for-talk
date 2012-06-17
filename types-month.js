30c8fd8321c9254d6a6bc357d83791a65dbb1483
Hibri Marzook <hibri@hibri.net>
2012-06-17 20:12:46 +0100

    Step 2,3

--- types-month.js
+++ types-month.js
@@ -1,43 +1,48 @@
-function renderTypesByMonth(){
+function renderTypesByMonth() {
 
-	
-	
-	
-	
-	var colors = ["#225533" ,"#44bbcc" ,"#88dddd" ,"#bbeeff"];
+
+	//parses the json
+	//stores in the form of
+	// CardData.year.month.Features
+	var cardTypesPerMonth = new CardData(teamData);
+
+
+
+	var colors = ["#225533", "#44bbcc", "#88dddd", "#bbeeff"];
 
 	var radius = 25;
 	var spacing = 55;
 
 	var xOffSet = 1;
 	var yOffSet = 1;
-	
+
 	var xPos = (radius + spacing) * xOffSet;
 	var yPos = (radius + spacing) * yOffSet;
 
-	var data = [25, 25, 25, 25];
+	
 	var year = 2012;
 	var month = 1;
 
+	var numberOfFeatures = cardTypesPerMonth.countFor(year, month - 1, "Feature");
+	var numberOfBugs = cardTypesPerMonth.countFor(year, month - 1, "Bug");
+	var numberOfMaintenanceTasks = cardTypesPerMonth.countFor(year, month - 1, "Build Maintenance");
+	var numberOfInfrastructureTasks = cardTypesPerMonth.countFor(year, month - 1, "Infrastructure");
+
+	var data = [numberOfFeatures, numberOfBugs, numberOfMaintenanceTasks, numberOfInfrastructureTasks];
 	drawPieForAMonth(year, month, xPos, yPos, radius, data, colors );
 
 
 
 }
 
-function drawPieForAMonth(year, month, xPos, yPos, radius, data, colors){
-	
-	
-	pie = r.piechart(xPos, yPos, radius, data , {colors: colors, label: data});
-	
-	
-	r.text(xPos, yPos +radius + 10, moment.monthsShort[month - 1] + " " + year  )
+function drawPieForAMonth(year, month, xPos, yPos, radius, data, colors) {
+
+
+	pie = r.piechart(xPos, yPos, radius, data, {
+		colors: colors,
+		label: data
+	});
+
+
+	r.text(xPos, yPos + radius + 10, moment.monthsShort[month - 1] + " " + year)
 }
-
-
-
-
-
-
-
-			
