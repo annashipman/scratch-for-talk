63b4614b02ad658d1a0214094554c0fcc237a6d4
Anna Shipman <annashipman@hotmail.co.uk>
2012-06-23 19:05:56 +0100

    changing colours

--- raphael/types_by_month.js
+++ raphael/types_by_month.js
@@ -1,7 +1,8 @@
 function init() {
 
-    var r = Raphael("holder");
+    var r = Raphael("holder"), 
+    colours = ["#225533", "#44bbcc", "#88dddd", "#bbeeff"];
     
-    r.piechart(125, 125, 100, [10,10,25,50])
+    r.piechart(125, 125, 100, [10,10,25,50], {colors:colours})
     
 }
