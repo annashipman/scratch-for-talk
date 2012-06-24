99e69228f2f9cc32413f60cd5434944be8d8b45f
Anna Shipman <annashipman@hotmail.co.uk>
2012-06-24 17:07:38 +0100

    plus line graph

--- raphael/types_by_month.js
+++ raphael/types_by_month.js
@@ -1,6 +1,6 @@
 function init() {
 
-    var r = Raphael("holder", 800, 800), 
+    var r = Raphael("holder", 2000, 800), 
     colours = ["#225533", "#44bbcc", "#88dddd", "#bbeeff"],
     radius = 25,
     line = 1,
@@ -10,18 +10,25 @@ function init() {
     labelOffset = radius + 10,
     sortedData = sort_data();
     
-   for (var year = 2010; year <= 2012; year++) {
-     for (var month = 1; month <= 12; month++) {
+    var lineX = [], lineYFeatures = [], lineYBugs = [], lineYInfrastructure = [];
+    
+  // for (var year = 2010; year <= 2012; year++) {
+     for (var month = 0; month <= 11; month++) {
+        var year = 2011;
         if (sortedData[year][month]) {
             features = sortedData[year][month]["MMF"] || 0,
             bugs = sortedData[year][month]["Bug"] || 0,
             infrastructure = sortedData[year][month]["Infrastructure"] || 0,
             maintenance = sortedData[year][month]["Maintenance"] || 0;
+            
+            lineX.push(month);
+            lineYFeatures.push(features);
+            lineYBugs.push(bugs);
+            lineYInfrastructure.push(infrastructure);
         
             var data = [features, bugs, infrastructure, maintenance];
         
-            r.piechart(x, y, radius, data, {colors:colours})
-            
+            r.piechart(x, y, radius, data, {colors:colours});
             }
         r.text(x, y + labelOffset, moment.monthsShort[month] + " " + year)
             
@@ -33,5 +40,17 @@ function init() {
 			y = pieSpacing * line;
 		}
       }
-    }
+  //  }
+    
+    x += pieSpacing;
+    line++;
+    y = pieSpacing * line;
+	var legendOptions = { 
+		legend: ["Features", "Bugs", "Infrastructure", "Maintenance"], 
+		legendpos: "east",  
+		label: [25, 25, 25, 25], 
+		colors: colours}
+	pie = r.piechart(x, y, radius, [25, 25, 25, 25], legendOptions);
+	
+   r.linechart(700, 60, 300, 220, lineX, [lineYFeatures, lineYBugs, lineYInfrastructure], { nostroke: false, axis: "0 0 1 1", symbol: "rect", smooth: true, colors: colours })
 } 
