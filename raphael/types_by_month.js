29a43f941d6ca6a508ef153a2cda18aa31ae7cec
Anna Shipman <annashipman@hotmail.co.uk>
2012-06-24 14:48:51 +0100

    lots of tidying up to do but here is the basic nightingale

--- raphael/types_by_month.js
+++ raphael/types_by_month.js
@@ -1,23 +1,37 @@
 function init() {
 
-    var r = Raphael("holder", 600, 800), 
+    var r = Raphael("holder", 800, 800), 
     colours = ["#225533", "#44bbcc", "#88dddd", "#bbeeff"],
     radius = 25,
+    line = 1,
     x = 50,
-    y = 50,
-    labelOffset = radius + 10;
-	
-    var sortedData = sort_data(),
-        year = 2012,
-        month = 0, //months go 0-11 in JS
-        features = sortedData[year][month]["MMF"] || 0,
-        bugs = sortedData[year][month]["Bug"] || 0,
-        infrastructure = sortedData[year][month]["Infrastructure"] || 0,
-        maintenance = sortedData[year][month]["Maintenance"] || 0;
+    pieSpacing = radius + 55,
+    y = pieSpacing * line,
+    labelOffset = radius + 10,
+    sortedData = sort_data();
     
-    var data = [features, bugs, infrastructure, maintenance];
-    
-    r.piechart(x, y, radius, data, {colors:colours})
-    r.text(x, y + labelOffset, moment.monthsShort[month] + " " + year)
-    
+   for (var year = 2010; year <= 2012; year++) {
+     for (var month = 1; month <= 12; month++) {
+        if (sortedData[year][month]) {
+            features = sortedData[year][month]["MMF"] || 0,
+            bugs = sortedData[year][month]["Bug"] || 0,
+            infrastructure = sortedData[year][month]["Infrastructure"] || 0,
+            maintenance = sortedData[year][month]["Maintenance"] || 0;
+        
+            var data = [features, bugs, infrastructure, maintenance];
+        
+            r.piechart(x, y, radius, data, {colors:colours})
+            
+            }
+        r.text(x, y + labelOffset, moment.monthsShort[month] + " " + year)
+            
+        x += pieSpacing;
+        
+        if ((month+1) % 6 == 0) {
+            x = 50;
+            line++;
+			y = pieSpacing * line;
+		}
+      }
+    }
 } 
