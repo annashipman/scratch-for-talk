f5c7b527f47c2f9cef8f7a8a2a6482ecc82e786f
Anna Shipman <anna.shipman@digital.cabinet-office.gov.uk>
2012-06-29 12:04:30 +0100

    basic state for nightingale

--- raphael/types_by_month.js
+++ raphael/types_by_month.js
@@ -1,8 +1,3 @@
 function init() {
-    var r = Raphael("holder", 600, 800);
-    
-    var data = [10, 10, 24, 50];
-    var colours = ["red", "green", "blue", "#bbeeff"];
-    r.piechart(125, 125, 100, data, {colors: colours});
     
 }
