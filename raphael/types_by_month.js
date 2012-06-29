838d4b602f4015d0fbfbe70c7df2f58b4d32053f
Anna Shipman <anna.shipman@digital.cabinet-office.gov.uk>
2012-06-29 12:26:45 +0100

    a few months

--- raphael/types_by_month.js
+++ raphael/types_by_month.js
@@ -1,21 +1,45 @@
 function init() {
-	
-	var r = Raphael("holder", 600, 800),
-		colours = ["#225533", "#44bbcc", "#88dddd", "#bbeeff"],
-		sortedData = sort_data(),
-		x = 125,
-		y = 125,
-		radius = 100,
-		labelOffset = radius + 10,
-		year = 2012,
-		month = 1, 
-        features = sortedData[year][month]["MMF"] || 0,
-        bugs = sortedData[year][month]["Bug"] || 0,
-        infrastructure = sortedData[year][month]["Infrastructure"] || 0,
-        maintenance = sortedData[year][month]["Maintenance"] || 0,
-        data = [features, bugs, infrastructure, maintenance];
+   var r = Raphael("holder", 2000, 800),
+       colours = ["#225533", "#44bbcc", "#88dddd", "#bbeeff"],
+        sortedData = sort_data(),
+        x = 50,
+        line = 1,
+        radius = 25,
+        labelOffset = radius + 10,
+        pieSpacing = radius + 55,
+        y = pieSpacing*line;
 
-		r.piechart(x, y, radius, data, {colors:colours})
-		r.text(x, y + labelOffset, moment.monthsShort[month] + " " + year);
+        for (var month = 0; month <= 11; month++) {
+            var year = 2011;
+                features = sortedData[year][month]["MMF"] || 0,
+                bugs = sortedData[year][month]["Bug"] || 0,
+                infrastructure = sortedData[year][month]["Infrastructure"] || 0,
+                maintenance = sortedData[year][month]["Maintenance"] || 0;
+    
+            var data = [features, bugs, infrastructure, maintenance];
+    
+            var pie = r.piechart(x, y, radius, data, {colors:colours}); 
+            
+            r.text(x, y + labelOffset, moment.monthsShort[month] + " " + year);
+            x += pieSpacing;
+            
+            if ((month+1) % 6 == 0) {
+                x = 50;
+                line++;
+		        y = pieSpacing * line;
+	        }
+        }
+      
+    x += pieSpacing;
+    line++;
+    y = pieSpacing * line;
+	
+	var legendOptions = { 
+		legend: ["Features", "Bugs", "Infrastructure", "Maintenance"], 
+		legendpos: "east",  
+		label: [25, 25, 25, 25], 
+		colors: colours}
+
+	pie = r.piechart(x, y, radius, [25, 25, 25, 25], legendOptions);
 
 }
