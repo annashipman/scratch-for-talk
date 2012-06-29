99a40f35014668c79ee3817bf146ef9398101903
Anna Shipman <anna.shipman@digital.cabinet-office.gov.uk>
2012-06-29 11:49:59 +0100

    basic state

--- jit/create_sunburst.js
+++ jit/create_sunburst.js
@@ -1,30 +1,3 @@
 function init(){
-    
-    var sb = new $jit.Sunburst({
-        injectInto: 'infovis',
-        Node: {
-            overridable: true
-        },
-        levelDistance: 90
-    });
-    
-    sb.loadJSON(json);
-    sb.refresh();
-    
-    
-    
-    
-    
 
-
-
-    var sb = new $jit.Sunburst({
-        injectInto: 'infovis',
-        Node: {
-            overridable: true
-        },
-   });
-    
-    sb.loadJSON(json);
-    sb.refresh();
 }
