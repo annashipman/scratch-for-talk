c285e0fbbee2f0d2e596205afc04da1547d3ca88
Anna Shipman <anna.shipman@digital.cabinet-office.gov.uk>
2012-06-30 20:31:59 +0100

    rewriting the hover functions as an actual programmer might write them

--- raphael/types_by_month.js
+++ raphael/types_by_month.js
@@ -3,15 +3,17 @@ function init() {
 
    var r = Raphael("holder", 2000, 800),
        colours = ["#225533", "#44bbcc", "#88dddd", "#bbeeff"],
-        sortedData = sort_data(),
-        x = 50,
-        line = 1,
-        radius = 25,
-        labelOffset = radius + 10,
-        pieSpacing = radius + 55,
-        y = pieSpacing*line;
+       sortedData = sort_data(),
+       flag;
+   
+   var x = 50,
+       line = 1,
+       radius = 25,
+       labelOffset = radius + 10,
+       pieSpacing = radius + 55,
+       y = pieSpacing*line;
  
-        var lineX = [], lineYFeatures = [], lineYBugs = [], lineYInfrastructure = [];
+	var lineX = [], lineYFeatures = [], lineYBugs = [], lineYInfrastructure = [];
         
         for (var month = 0; month <= 11; month++) {
             var year = 2011;
@@ -23,7 +25,16 @@ function init() {
             var data = [features, bugs, infrastructure, maintenance];
     
             var pie = r.piechart(x, y, radius, data, {colors:colours}); 
-            pie.hover(hoverIn, hoverOut);
+            
+            pie.hover(
+            	//hover in	
+            	function() { 
+            		flag = r.flag(this.mx, this.my, this.value.value) 
+            	}, 
+            	//hover out
+            	function(){ 
+            		flag.remove()
+            	});	
             
             lineX.push(month);
             lineYFeatures.push(features);
@@ -57,45 +68,4 @@ function init() {
 	 	smooth: true, 
 	 	colors: colours })
 
-function hoverIn() {
-	this.sector.stop();
-	this.flag = r.popup(this.mx, this.my, this.value.value).attr({
-		fill: "0-#c9de96-#8ab66b:44-#398235",
-		stroke: "#000"
-	});
-	this.sector.scale(1.5, 1.5, this.cx, this.cy);
-
-	if (this.label) {
-		this.label[0].stop();
-		this.label[0].attr({
-			r: 7.5
-		});
-		this.label[1].attr({
-			"font-weight": 800
-		});
-	}
 }
-
-function hoverOut() {
-	this.flag.animate({
-		opacity: 0
-	}, 300, function() {
-		this.remove();
-	});
-	this.sector.animate({
-		transform: 's1 1 ' + this.cx + ' ' + this.cy
-	}, 500, "bounce");
-	if (this.label) {
-		this.label[0].animate({
-			r: 5
-		}, 500, "bounce");
-		this.label[1].attr({
-			"font-weight": 400
-		});
-	}
-}
-      
-    
-
-
-} 
