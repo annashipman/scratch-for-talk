d7c3b5e1476e83cfc5d7b43ae6834d712d96797e
Anna Shipman <anna.shipman@digital.cabinet-office.gov.uk>
2012-07-01 04:48:26 +0100

    working on javascript data sorter...

--- raphael/data_sorter.js
+++ raphael/data_sorter.js
@@ -3,22 +3,47 @@ function sort_data() {
     var data = team_data; 
     
     var sortedData = [];
-    
+
+    console.log("hiya anna!");
+
+    //get all the dates from the data
     data.forEach(function(task) { 
         var dateFinished = moment(task["In Production"], "DD/MM/YYYY");
+        
+        
         if (dateFinished) {
             var month = dateFinished.month() 
-                year = dateFinished.year(),
-                taskType = task["Type"];
+                year = dateFinished.year();
+            var monthString = moment.monthsShort[month] + " " + year;
+
+            if(sortedData.indexOf(monthString) === -1) {
+                sortedData.push(monthString);
+            }
+        }    
+    });
+
+    console.log(sortedData)
+
+    //go through the 
+
+        // if sortedData
+
+    
+ //    data.forEach(function(task) { 
+ //        var dateFinished = moment(task["In Production"], "DD/MM/YYYY");
+ //        if (dateFinished) {
+ //            var month = dateFinished.month() 
+ //                year = dateFinished.year(),
+ //                taskType = task["Type"];
         
-		    if(sortedData[year] == undefined) sortedData[year] = new Object(); 
-		    if(sortedData[year][month] == undefined) sortedData[year][month] = new Object();
-		    if(sortedData[year][month][taskType] == undefined) sortedData[year][month][taskType] = 0;
+	// 	    if(sortedData[year] == undefined) sortedData[year] = new Object(); 
+	// 	    if(sortedData[year][month] == undefined) sortedData[year][month] = new Object();
+	// 	    if(sortedData[year][month][taskType] == undefined) sortedData[year][month][taskType] = 0;
 		    
-		    sortedData[year][month][taskType] ++; 
-        }
-	});
+	// 	    sortedData[year][month][taskType] ++; 
+ //        }
+	// });
 	
-	return sortedData;
+	// return sortedData;
 }
 
