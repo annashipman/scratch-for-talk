ccbbf81d550977b5989e2b066d1b6c9524fddc39
Anna Shipman <anna.shipman@digital.cabinet-office.gov.uk>
2012-07-01 20:27:38 +0100

    data sorter

--- raphael/data_sorter.js
+++ raphael/data_sorter.js
@@ -1,26 +1,34 @@
 function sort_data() {
 
-    var data = team_data; 
-    
-    var sortedData = [];
-
-    console.log("hiya anna!");
-    var index = {};
+    var data = team_data, 
+        sortedData = [],
+        index = {};
 
 
     //get all the dates from the data
     data.forEach(function(task) { 
-        var dateFinished = moment(task["In Production"], "DD/MM/YYYY");
+        var dateFinished = moment(task["In Production"], "DD/MM/YYYY"); //lines not longer thank this
         
         if (dateFinished) {
             var month = dateFinished.month() 
                 year = dateFinished.year(),
                 date_string = month+"-"+year;
-                console.log(typeof(month) + " " + typeof(year));
 
-            if(index[date_string] !== undefined) {
-                var data = sortedData[index[date_string]];
-                // set data. features ./ bugs right...
+            if(sortedData[index[date_string]] !== undefined) {
+                var data = sortedData[index[date_string]],
+                    taskType = task.Type;
+                
+                console.log(taskType);
+                if ("MMF"===taskType){
+                    data.features++;
+                } else if ("Bug"===taskType){
+                    data.bug++;
+
+                    //TODO am here - do these in the right order. 
+                } else if ("Infrastructure"){
+                    data.infrastructure++;
+                } else if 
+                //console.log(sortedData[index[date_string]])
             } else {
                 var monthString = moment.monthsShort[month] + " " + year;
                 var monthData = {
@@ -29,22 +37,23 @@ function sort_data() {
                     monthString: monthString,
                     features: 0,
                     bugs: 0,
-                    toString: function() {return this.monthString;}
+                    infrastructure: 0,
+                    maintenance: 0
+                    // toString: function() {return this.monthString;}
                 }
                 index[date_string] = sortedData.push(monthData); 
             }
         }    
     });
 
-    console.log("before sort", sortedData.join(" , "));
+    //console.log("before sort", sortedData.join(" , "));
     var anotherArray = sortedData.sort(function(a, b) {
         if (a.year === b.year) return a.month - b.month;
         return a.year - b.year;}
     );
 
-    console.log(sortedData.join(" , "));
+    //console.log(sortedData.join(" , "));
     console.log(sortedData);
-
 
     //go through the 
 
